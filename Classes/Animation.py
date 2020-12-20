0efcd848cc9df5e052e7f804f118b069e718e20b
Александр Лебедев <sashaleb2006@yandex.ru>
2020-12-20 18:05:25 +0300

    Перенес к себе анимацию на переработку

---
+++ Classes/Animation.py
@@ -0,0 +1,54 @@
+import pygame
+import os
+import sys
+import time
+
+
+SIZE = (500, 500)
+
+
+class SpriteAnimation(pygame.sprite.Sprite):
+    frames = pygame.sprite.Group()
+    
+    def __init__(self, *group, x, y, original, actions):
+        super().__init__(*group)
+        self.original = original.image
+        self.rect = self.original.get_rect() # Прямоугольник
+        self.x = x
+        self.y = y
+        # Действия
+        self.actions = actions
+        self.action_now = self.actions.keys()[0]
+        self.time = self.actions[self.actions.keys()[0]]['frames'][1]
+        # Таймер
+        self.MYEVENTTYPE = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.MYEVENTTYPE, self.time)
+        
+    def draw(self, screen, action): # Анимация
+        image = pygame.image.load(self.actions[self.action_now]['frames'][0]) # Загрузка изображения
+        self.time = self.actions[self.action_now]['frames'][1]
+        pygame.time.set_timer(self.MYEVENTTYPE, self.time)
+        
+    def update(self, camera, rotation, *events): # Изменение параметров
+        # Изменение размера картинки
+        w = int(self.rect.width * camera.get_zoom())
+        h = int(self.rect.height * camera.get_zoom())
+        self.original = pygame.transform.scale(self.original, (w, h))
+        self.rect.width *= camera.get_zoom()
+        self.rect.height *= camera.get_zoom()
+        
+        # Вращение изображения с прямоугольником
+        rot_image = pygame.transform.rotate(self.original, rotation)
+        rot_rect = self.rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        self.original = rot_image.subsurface(rot_rect).copy()      
+        
+        # Перемещение изображения относительно камеры
+        x = SIZE[0] / 2 + (self.x - self.rect.width / 2 - camera.get_position()[0]) * camera.get_zoom()
+        y = SIZE[1] / 2 + (self.y - self.rect.height / 2 - camera.get_position()[1]) * camera.get_zoom()
+        self.rect.center = (x, y)
+        
+        # Таймер кадров
+        for e in events:
+            if e.type == self.MYEVENTTYPE:
+                self.action_now = self.actions[self.action_now]['next']
