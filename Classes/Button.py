35e6b37fda5e3e4af7277dc23b454f810acb39ce
kartofagen <74975693+kartofagen@users.noreply.github.com>
2021-01-16 20:52:21 +0300

    Изменил класс для удобной работы

---
+++ Classes/Button.py
@@ -0,0 +1,76 @@
+import pygame
+from math import pi
+
+
+class Button:
+    def __init__(self, text, text_color, text_size, font,
+                 x, y, w, h, colors, rounding=False):
+        self.blocked = False
+        
+        self.text = text
+        self.text_size = text_size
+        self.text_color = text_color
+        self.font = font
+        
+        self.colors = colors
+        self.color = self.colors[0]
+        
+        self.rounding = rounding
+
+        # Сохранение области нажатия
+        if rounding: # Круглая ли кнопка
+            self.rect = pygame.Rect(x - h / 2, y, w + h, h)
+        else:
+            self.rect = pygame.Rect(x, y, w, h)
+
+        # Переменные размеров
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+        # Значения нажатия
+        self.on = False
+        self.down = False
+
+    def animation(self): # Анимация при нажатии
+        if len(self.colors) == 4 and self.blocked:
+            self.color = self.colors[3]
+        elif self.on and self.down:
+            self.color = self.colors[2]
+        elif self.on:
+            self.color = self.colors[1]
+        else:
+            self.color = self.colors[0]
+
+    def drawing(self, screen):  # Нарисовать кнопку
+        self.animation() # Запуск анимации
+
+        # Рисование кнопки
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h), 0)
+        if self.rounding:
+            pygame.draw.circle(screen, self.color, (self.x, self.y + self.h / 2), self.h / 2)
+            pygame.draw.circle(screen, self.color, (self.x + self.w, self.y + self.h / 2), self.h / 2)
+
+        # Отрисовка текста
+        to_write = pygame.font.SysFont(self.font, self.text_size).render(self.text, 1, self.text_color)
+        screen.blit(to_write, ((self.x + self.w / 2) - to_write.get_width() / 2, (self.y + self.h / 2) - to_write.get_height() / 2))
+        
+    def block(self, value):
+        self.blocked = value
+
+    def selected(self, value): # Ниже функции для нажатия
+        if not value:
+            self.down = False
+        # self.down = False
+        self.on = value
+
+    def on_button(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            return True
+        else:
+            return False
+
+    def pressed(self, value):
+        if not self.blocked:
+            self.down = value
